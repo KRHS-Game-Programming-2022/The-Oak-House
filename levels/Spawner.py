c8cda5613e9c186481dda216019b75a45e707c68
Mason M Mason <guitarguy748@gmail.com>
2022-02-18 09:47:02 -0500

    started levels

---
+++ levels/Spawner.py
@@ -0,0 +1,11 @@
+import pygame, sys, math
+
+class Spawner():
+    def __init__(self, pos=[650,480]):
+        self.image = pygame.image.load("Images/WallTiles/Spawner.png")
+        self.rect = self.image.get_rect(center = pos)
+        self.kind = "Spawner"
+       
+       
+    def update(self, size):
+       pass
--- main.py
+++ main.py
@@ -1,1 +1,40 @@
+import pygame, sys, math
+from WallTile import *
+from Spawner import *
+def loadLevel (lev):
+    f = open(lev, 'r')
+    lines = f.readlines()
+    f.close()
+   
+    size = 50
+    offset = size/1
+    tiles = []
+    newLines = []
+    walls = []
+    spawners = []
+   
+    for line in lines:
+        newLine = ""
+        for c in line:
+            if c != "\n":  
+                newLine += c
+        newLines += [newLine]
+       
+    lines = newLines
+   
+    for y, line in enumerate(lines):
+        for x, c in enumerate(line):
+            if c == "#":
+                walls += [Wall([x*size+offset, y*size+offset])]
+            if c == "X":
+                spawners += [Spawner([x*size+offset, y*size+offset])]
+    tiles = [walls,
+            spawners]
+    return tiles
+   
+   
+
+
+#loadLevel("Levels/example.lvl")
+
 
